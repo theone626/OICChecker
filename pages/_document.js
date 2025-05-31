73e628fcee0400536b4be298e9741171c7344c14
Demetri Chambers <your.email@example.com>
2025-05-31 12:52:32 -0400

    Add Google site verification meta tag

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="google-site-verification" content="AKFdBhMGDGCP_BwdrbBWerO3gvTCmfx1lXTFU8PVVak" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+} 
